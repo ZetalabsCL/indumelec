8a3527d0624daacfeb46e2f8425dae417cc732ad
Jorge Romero <jorge.a.romerox@gmail.com>
2019-03-11 08:43:02 -0400

    Initial Commit

---
+++ indumelec-web/src/main/java/com/zetalabs/indumelec/web/IndumelecWebApplication.java
@@ -0,0 +1,13 @@
+package com.zetalabs.indumelec.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndumelecWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndumelecWebApplication.class, args);
+	}
+
+}
