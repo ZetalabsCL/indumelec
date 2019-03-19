a458113e91e855f3ff0ec9586a709e62453509d2
Jorge <jorge@zetalabs.cl>
2019-03-18 20:26:48 -0400

    Update Api

--- src/main/java/com/zetalabs/indumelec/controller/ApiController.java
+++ src/main/java/com/zetalabs/indumelec/controller/ApiController.java
@@ -3,6 +3,9 @@ package com.zetalabs.indumelec.controller;
 import com.zetalabs.indumelec.model.Company;
 import com.zetalabs.indumelec.service.CompanyService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,13 +16,17 @@ public class ApiController {
     private CompanyService companyService;
 
     @RequestMapping("/api/company/search")
-    public Company greeting(@RequestParam("taxId") String taxId) {
+    public ResponseEntity greeting(@RequestParam("taxId") String taxId) {
         Company company = companyService.getCompanyByTaxId(taxId);
 
-        if (company==null){
-            company = new Company();
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/json");
+        ResponseEntity responseEntity = new ResponseEntity<>(headers, HttpStatus.OK);
+
+        if (company != null) {
+            responseEntity = new ResponseEntity<>(company, headers, HttpStatus.OK);
         }
 
-        return company;
+        return responseEntity;
     }
 }
