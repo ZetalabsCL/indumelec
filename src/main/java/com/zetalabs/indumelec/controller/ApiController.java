f0c03b4e27edefe78cfd958cfcf8038bc54e86a3
Jorge <jorge@zetalabs.cl>
2019-04-08 23:18:59 -0400

    Refactor update layout

--- src/main/java/com/zetalabs/indumelec/controller/ApiController.java
+++ src/main/java/com/zetalabs/indumelec/controller/ApiController.java
@@ -10,22 +10,41 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class ApiController {
     @Autowired
     private CompanyService companyService;
 
     @RequestMapping("/api/company/search")
-    public ResponseEntity greeting(@RequestParam("taxId") String taxId) {
+    public ResponseEntity companySearch(@RequestParam("taxId") String taxId) {
+        ResponseEntity responseEntity = getResponse();
         Company company = companyService.getCompanyByTaxId(taxId);
 
+        if (company != null) {
+            responseEntity = new ResponseEntity<>(company, responseEntity.getHeaders(), HttpStatus.OK);
+        }
+
+        return responseEntity;
+    }
+
+    @RequestMapping("/api/company/list")
+    public ResponseEntity companyList() {
+        ResponseEntity responseEntity = getResponse();
+        List<Company> companyList = companyService.getCompanyList();
+
+        if (companyList != null) {
+            responseEntity = new ResponseEntity<>(companyList, responseEntity.getHeaders(), HttpStatus.OK);
+        }
+
+        return responseEntity;
+    }
+
+    private ResponseEntity getResponse(){
         HttpHeaders headers = new HttpHeaders();
         headers.add("Content-Type", "application/json");
         ResponseEntity responseEntity = new ResponseEntity<>(headers, HttpStatus.OK);
-
-        if (company != null) {
-            responseEntity = new ResponseEntity<>(company, headers, HttpStatus.OK);
-        }
 
         return responseEntity;
     }
