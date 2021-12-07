42fd6a86422dcdc65db7a66349b9acb6f26c499c
Jorge Romero <jorge.a.romerox@gmail.com>
2021-12-07 16:51:48 -0500

    Add Report Generation

--- src/main/java/com/zetalabs/indumelec/controller/ReportController.java
+++ src/main/java/com/zetalabs/indumelec/controller/ReportController.java
@@ -1,17 +1,46 @@
 package com.zetalabs.indumelec.controller;
 
+import com.zetalabs.indumelec.service.ReportService;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Controller
 public class ReportController {
+    private final ReportService reportService;
+
+    public ReportController(ReportService reportService){
+        this.reportService = reportService;
+    }
+
     @RequestMapping(value={"/report/dashboard"}, method = RequestMethod.GET)
     public ModelAndView dashboard(){
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("report/dashboard");
 
         return modelAndView;
+    }
+
+    @RequestMapping(value={"/report/download"}, method = RequestMethod.POST)
+    public ResponseEntity<ByteArrayResource> download(@RequestParam("deliveryDateFrom") String from,
+                                                      @RequestParam("deliveryDateTo") String to,
+                                                      @RequestParam("priorityType") String priority){
+        byte[] data = reportService.getReport(from, to, priority);
+        ByteArrayResource resource = new ByteArrayResource(data);
+        String currentDateTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("ddMMyy_HHmmss"));
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION,"attachment;filename=\"reporte_" + currentDateTime + ".xlsx\"" )
+                .contentType(MediaType.APPLICATION_PDF).contentLength(data.length)
+                .body(resource);
     }
 }
--- src/main/java/com/zetalabs/indumelec/repository/QuoteRepository.java
+++ src/main/java/com/zetalabs/indumelec/repository/QuoteRepository.java
@@ -6,12 +6,17 @@ import com.zetalabs.indumelec.model.types.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface QuoteRepository extends JpaRepository<Quote, Long> {
     List<Quote> getQuotesByStatusEqualsOrderByDeliveryDate(Status status);
 
     List<Quote> getQuotesByStatusEqualsAndPriorityTypeEqualsOrderByDeliveryDate(Status status, PriorityType priorityType);
+
+    List<Quote> getQuotesByDeliveryDateBetween(LocalDate from, LocalDate to);
+
+    List<Quote> getQuotesByDeliveryDateBetweenAndPriorityType(LocalDate from, LocalDate to, PriorityType priorityType);
 
     @Query("SELECT q FROM Quote q WHERE q.status in (4,5,6,7) order by q.deliveryDate")
     List<Quote> getInProgressQuotes();
